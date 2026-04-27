d3890222226bb803822848f6d78b8453e0936da4
nieeehlsss <4bvpthfbvz@privaterelay.appleid.com>
2026-04-27 12:58:54 +0200

    3 endpoints

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # API Project
 
+uv initialisieren: uv init
+Fastapi Server starten: uv run fastapi dev
--- main.py
+++ main.py
@@ -5,3 +5,12 @@ app = FastAPI()
 @app.get("/")
 def root():
     return {"message": "Hello, World!"}
+
+@app.get("/name/{name}")
+def greet_name(name: str):
+    return {"message": f"Hello, {name}!"}
+
+@app.get("/calculate/{number}")
+def calculate(number: float):
+    result = number * 2 + 5
+    return {"message": f"Der verrechnete Wert von {number} ist {result}"}
